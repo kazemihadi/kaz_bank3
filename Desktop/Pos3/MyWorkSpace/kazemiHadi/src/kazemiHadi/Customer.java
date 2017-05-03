ad85674d7390651bbe3d1a30fd334523b1b251e5
Hadi <hadi@hadis-mbp>
2017-05-03 20:07:46 +0200

    ich habe was geaendert.

--- Desktop/Pos3/MyWorkSpace/kazemiHadi/src/kazemiHadi/Customer.java
+++ Desktop/Pos3/MyWorkSpace/kazemiHadi/src/kazemiHadi/Customer.java
@@ -1,5 +1,33 @@
 package kazemiHadi;
 
 public class Customer {
+	
+	private int[][] wert;
+	
+	public Customer()
+	{
+		wert = new int[2][3];
+		
+		wert[0][0] = 9;
+		wert[0][1] = 10;
+		wert[0][2] = 19;
+		
+		print();
+	}
+	
+	public void print()
+	{
+		System.out.println(wert[0][0]);
+	}
+	
+	
+	
+	
+	public static void main(String[] args) 
+	{
+		new Customer();
+	}
+	
+	
 
 }
